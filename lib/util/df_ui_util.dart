f2e7d2de3858db0506895b24bc63077e932b7b6c
ym6745476 <ym6745476@163.com>
2021-08-17 18:30:11 +0800

    增加弹窗框页面

---
+++ lib/util/df_ui_util.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+/// UI工具类
+class DFUiUtil {
+  ///显示弹出框
+  static showLayer(BuildContext context, Widget child){
+    showDialog<Null>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return child;
+      },
+    ).then((val) {
+      print(val);
+    });
+  }
+}
